06d3250be5411ce9163c6a53991c5443b48a30ab
webfaisalbd <web.faisal.bd@gmail.com>
2022-03-02 00:47:10 +0600

    display search result

--- app.js
+++ app.js
@@ -9,8 +9,27 @@ searchButton.addEventListener('click',function(){
     searchField.value='';
     // load meals from mealdb url 
     const url=`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
-    console.log(url);
+    // console.log(url);
     fetch(url)
     .then(res =>res.json())
-    .then(data =>console.log(data.meals))
+    .then(data =>displaySearchResult(data.meals))
 }) 
+
+
+// display search result 
+     const displaySearchResult= meals => {
+     const searchResult=document.getElementById('search-result');
+     meals.forEach(meal=> {
+         console.log(meal);
+        const div=document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML=`<div class="card">
+        <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${meal.strMeal}</h5>
+          <p class="card-text">${meal.strInstructions.slice(0,100)}</p>
+        </div>
+      </div>`;
+      searchResult.appendChild(div);
+     })
+}
