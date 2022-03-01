fb79242c8364ca1ec1a740ff95777088e9c31e07
webfaisalbd <web.faisal.bd@gmail.com>
2022-03-02 01:36:13 +0600

    dynamic url based on click and display single meal details

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const searchField=document.getElementById('search-field');
 const searchButton=document.getElementById('search-button');
+const searchResult=document.getElementById('search-result');
 
 searchButton.addEventListener('click',function(){
     
@@ -18,18 +19,50 @@ searchButton.addEventListener('click',function(){
 
 // display search result 
      const displaySearchResult= meals => {
-     const searchResult=document.getElementById('search-result');
+    //  const searchResult=document.getElementById('search-result');
      meals.forEach(meal=> {
-         console.log(meal);
+        //  console.log(meal);
         const div=document.createElement('div');
         div.classList.add('col');
         div.innerHTML=`<div class="card">
         <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${meal.strMeal}</h5>
-          <p class="card-text">${meal.strInstructions.slice(0,100)}</p>
+          <button onClick="loadDetails(${meal.idMeal})" class="btn-success rounded">Meal Details</button>
         </div>
       </div>`;
       searchResult.appendChild(div);
      })
+}
+
+
+// load details meal 
+const loadDetails=(mealId)=>{
+// console.log(meal);
+const url=`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+fetch(url)
+.then(res=>res.json())
+.then(data=>showLoadDetails(data.meals[0]))
+}
+
+
+// show details meal 
+const showLoadDetails= mealDetail => {
+    console.log(mealDetail);
+    const singleLoadDetails=document.getElementById('singleLoadDetails');
+    const div=document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML=`<div class="card">
+        <img src="${mealDetail.strMealThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${mealDetail.strMeal}</h5>
+          <p>${mealDetail.strCategory}</p>
+          <p>${mealDetail.strArea}</p>
+          <p>${mealDetail.strInstructions.slice(0,150)}</p>
+
+        </div>
+      </div>`;
+    singleLoadDetails.appendChild(div);
+    // remove child 
+    searchResult.remove();         
 }
